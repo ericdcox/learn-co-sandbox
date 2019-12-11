9c0bcb696c82b52e6c0ea457ce6a308e764a1c29
Eric Cox <ericdcox@icloud.com>
2019-12-11 19:46:42 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,10 @@
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer("Maria", "Ruby")
+
+ 
+greeting_programmer("Steven",)
+
---
+++ run_code_inside.rb
@@ -0,0 +1,8 @@
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  p "Guy Fieri"
+end
+
+stylish_chef
